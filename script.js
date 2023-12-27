52968c85706cbb8ebb18e1f7a68078d843ad35bf
killian31 <80256683+killian31@users.noreply.github.com>
2023-12-28 00:18:53 +0100

    feat: added hero

---
+++ script.js
@@ -0,0 +1,26 @@
+document.addEventListener('scroll', function() {
+    if (window.scrollY > 50) {
+        document.body.classList.add('scrolled');
+    } else {
+        document.body.classList.remove('scrolled');
+    }
+});
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("back-to-top-btn").style.display = "block";
+    } else {
+        document.getElementById("back-to-top-btn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+document.getElementById("back-to-top-btn").onclick = function() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+};
