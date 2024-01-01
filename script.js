4fd66095a4cc657503081b071916364687d33dfe
killian31 <80256683+killian31@users.noreply.github.com>
2024-01-02 00:08:08 +0100

    fix: burger menu phone

--- script.js
+++ script.js
@@ -24,3 +24,7 @@ document.getElementById("back-to-top-btn").onclick = function() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
 };
+
+document.getElementById('navbar-toggle').addEventListener('click', function() {
+    document.querySelector('.navbar-links').classList.toggle('responsive');
+});
